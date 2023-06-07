989864e3f0a6594f36a23e9df6d2a9abaa5f6f66
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-07 17:16:46 +0900

    feat : Canvas
    UI 가제작, Rayout Group 사용

--- Assets/Managers/UiManager.cs
+++ Assets/Managers/UiManager.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
-    
+    private static EventSystem eventSystem;
+    private void Awake()
+    {
+        eventSystem = GameManager.Resource.Instantiate<EventSystem>("UI/EventSystem");
+        eventSystem.transform.parent = transform;
+        eventSystem.name = "EventSystem";
+    }
 }
